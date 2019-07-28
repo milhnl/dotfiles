9ac568c5f58ea27d409ef961bd113c4a11181435
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2019-07-28 21:37:06 +0200

    Set correct EDITOR on macOS

--- XDG_CONFIG_HOME/shell/profile.sh
+++ XDG_CONFIG_HOME/shell/profile.sh
@@ -35,11 +35,13 @@ fi
 set +a
 
 # EDITOR ----------------------------------------------------------------------
-if (which vis && [ "$OS" != Darwin ] || vis -v) >/dev/null 2>/dev/null; then
+if [ "$OS" = Darwin ] && which vise >/dev/null 2>&1; then
+    export EDITOR=vise
+elif (which vis && [ "$OS" != Darwin ] || vis -v) >/dev/null 2>&1; then
     export EDITOR=vis
-elif which nvim >/dev/null 2>/dev/null; then
+elif which nvim >/dev/null 2>&1; then
     export EDITOR=nvim
-elif which vim >/dev/null 2>/dev/null; then
+elif which vim >/dev/null 2>&1; then
     export EDITOR=vim
 fi
 

26a34ac45be8b02aab1949e2a7f2a426f7a6ffa2
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2022-09-01 13:38:27 +0200

    Move media-session configuration to wireplumber

---
+++ XDG_CONFIG_HOME/wireplumber/main.lua.d/51-device-rename.lua
@@ -0,0 +1,12 @@
+rules = {
+  { "Tiger Lake*Speaker ? Headphones", "Internal" },
+  { "Tiger Lake*DisplayPort 1 Output", "HDMI 1" },
+  { "Tiger Lake*DisplayPort 2 Output", "HDMI 2" },
+  { "Tiger Lake*DisplayPort 3 Output", "HDMI 3" },
+}
+for _, rule in ipairs(rules) do
+  table.insert(alsa_monitor.rules, {
+    matches = { { { "node.description", "matches", rule[1] } } },
+    apply_properties = { ["node.description"] = rule[2] },
+  })
+end

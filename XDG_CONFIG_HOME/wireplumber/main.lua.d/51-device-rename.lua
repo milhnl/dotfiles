7a6ef0161ec835490ae1a5ec4ed196046ce6df64
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2023-04-14 10:42:25 +0200

    Update wireplumber device renaming rules

--- XDG_CONFIG_HOME/wireplumber/main.lua.d/51-device-rename.lua
+++ XDG_CONFIG_HOME/wireplumber/main.lua.d/51-device-rename.lua
@@ -1,5 +1,7 @@
 rules = {
   { "Tiger Lake*Speaker ? Headphones", "Internal" },
+  { "Tiger Lake*Controller Headphones", "Headphones" },
+  { "Tiger Lake*Controller Speaker", "Speaker" },
   { "Tiger Lake*DisplayPort 1 Output", "HDMI 1" },
   { "Tiger Lake*DisplayPort 2 Output", "HDMI 2" },
   { "Tiger Lake*DisplayPort 3 Output", "HDMI 3" },

e517affd26cc2546b3875301ceeeeb1a51d2f7b3
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2023-05-28 12:48:05 +0200

    Remove unnecessary mqtt logging from home

--- XDG_CONFIG_HOME/vis/lexers/git-commit.lua
+++ XDG_CONFIG_HOME/vis/lexers/git-commit.lua
@@ -1,17 +1,11 @@
-local l = require('lexer')
-local token = l.token
-local S = lpeg.S
-local diff = l.load('diff')
+local lexer = require('lexer')
+local token, word_match = lexer.token, lexer.word_match
+local P, S = lpeg.P, lpeg.S
 
-local M = {_NAME = 'git-commit'}
+local lex = lexer.new('git-commit', {lex_by_line = true})
+local diff = lexer.load('diff')
 
-local comment = token(l.COMMENT, '#' * l.any^0)
+lex:add_rule('comment', token(lexer.COMMENT, '#' * lexer.any^0))
+lex:add_rule('any_line', token(lexer.DEFAULT, lexer.any^1))
 
-local _rules = diff._rules
-table.insert(_rules, #_rules - 1, {'comment', comment})
-M._rules = _rules
-M._tokenstyles = diff._tokenstyles
-M._foldsymbols = diff._foldsymbols
-M._LEXBYLINE = diff._LEXBYLINE
-
-return M
+return lex

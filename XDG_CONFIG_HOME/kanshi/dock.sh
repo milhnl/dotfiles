c36e0ec0b29c0727f06637dda6e2ca7dd726aa75
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2023-08-22 09:41:29 +0000

    Set external screen scaling correctly on PinePhone

--- XDG_CONFIG_HOME/kanshi/dock.sh
+++ XDG_CONFIG_HOME/kanshi/dock.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 daemon() ( exec "$@" >/dev/null 2>&1 & ); \
 
+swaymsg output "'GIGA-BYTE TECHNOLOGY CO., LTD. M28U 22170B000449' `
+    `scale $(swaymsg -t get_outputs | jq -r '.[]
+        | select("\(.make) \(.model) \(.serial)"
+            == "GIGA-BYTE TECHNOLOGY CO., LTD. M28U 22170B000449")
+        | .modes | max_by(.width).width / 3840 * 2')"
+
 sudo systemctl start avahi-daemon;
 ps -Aocomm | grep -qxF nqptp || daemon sudo nqptp
 sleep 1

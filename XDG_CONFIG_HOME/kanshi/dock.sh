5d4a1dddcf5d7c099b67827569a1d6c4470f1fb9
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2023-08-22 09:40:39 +0000

    Start avahi-daemon when starting shairport-sync

--- XDG_CONFIG_HOME/kanshi/dock.sh
+++ XDG_CONFIG_HOME/kanshi/dock.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 daemon() ( exec "$@" >/dev/null 2>&1 & ); \
 
+sudo systemctl start avahi-daemon;
 ps -Aocomm | grep -qxF nqptp || daemon sudo nqptp
 sleep 1
 ps -Aocomm | grep -qxF shairport-sync || daemon shairport-sync -o pa

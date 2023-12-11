7e75de611d563dbd026d7a6336e06d76f0ac5d52
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2023-12-11 10:37:52 +0000

    Enable editing from git in Windows

--- XDG_CONFIG_HOME/powershell/profile.ps1
+++ XDG_CONFIG_HOME/powershell/profile.ps1
@@ -4,4 +4,9 @@ function dot {
   git -C "$env:DOTFILES" @Args
 }
 
+if (([System.Environment]::OSVersion.Platform -eq "Win32NT") `
+    -or ($PSVersionTable.Platform -eq "Windows")) {
+  $env:EDITOR="wsl -- sh -lic 'editor `"\`$(wslpath -u `"\$@`")`"' --"
+}
+
 Set-Alias w Enter-Workspace

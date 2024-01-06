607eea1f6585be122055cf54028d6c51d7063aea
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2024-01-06 15:55:40 +0100

    Reformat Hammerspoon config file

--- XDG_CONFIG_HOME/hammerspoon/init.lua
+++ XDG_CONFIG_HOME/hammerspoon/init.lua
@@ -10,21 +10,12 @@ hs.window.filter.ignoreAlways['Electron Helper (Renderer)'] = true
 hs.window.filter.ignoreAlways['Mail Networking'] = true
 configWatcher = hs.pathwatcher.new(hs.configdir, hs.reload):start()
 
-function runOrRaise(bundleID, names)
-    local windows = hs.window.filter.new(names):getWindows()
-    if windows == nil or next(windows) == nil then
-        hs.application.launchOrFocusByBundleID(bundleID)
-    else
-        windows[1]:focus()
-    end
-end
-
 -- Cmd + I
-hs.hotkey.bind({"cmd"}, "I", function()
-    hs.application.launchOrFocusByBundleID("com.apple.Safari")
+hs.hotkey.bind({ 'cmd' }, 'I', function()
+  hs.application.launchOrFocusByBundleID('com.apple.Safari')
 end)
 
 -- Cmd + D
-hs.hotkey.bind({"cmd"}, "D", function()
-    hs.application.launchOrFocusByBundleID("com.microsoft.vscode")
+hs.hotkey.bind({ 'cmd' }, 'D', function()
+  hs.application.launchOrFocusByBundleID('com.microsoft.vscode')
 end)

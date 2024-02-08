dbd62da25668ed45758df480ca9b7b86e31cb06b
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2024-02-08 11:22:10 +0100

    Change bindings in visrc from closures to commands

--- XDG_CONFIG_HOME/vis/visrc.lua
+++ XDG_CONFIG_HOME/vis/visrc.lua
@@ -143,6 +143,29 @@ if lspc then
     vis:command('lspc-back')
   end)
 end
+
+vis:command_register('format', function(argv, force, win, selection, range)
+  if format.formatters[vis.win.syntax] then
+    win.selection.pos = format.apply(win.file, range, pos)
+  elseif lspc then
+    local lspc_conf = lspc.ls_map[win.syntax]
+    if lspc_conf then
+      local ls = lspc.running[lspc_conf.name]
+      if ls and ls.capabilities['documentFormattingProvider'] then
+        vis:command('lspc-format')
+      end
+    end
+  else
+    vis:info('No formatter for ' .. win.syntax)
+  end
+end, 'Format file using configured formatter')
+vis:operator_new('=', function()
+  vis:command('format')
+end)
+vis:map(vis.modes.NORMAL, '=', function()
+  vis:command('format')
+end)
+
 vis.ftdetect.filetypes.mail = nil
 vis.ftdetect.filetypes.beancount = {
   ext = { '%.bean$', '%.beancount$' },
@@ -156,6 +179,131 @@ table.insert(
   '.tsx?$'
 )
 table.insert(vis.ftdetect.filetypes.xml.ext, '.csproj$')
+
+vis:command_register('fuzzy-open', function(argv, force, win, sel, range)
+  local fz = io.popen(
+    'tput cup $(( $(tput lines) - 10)) >/dev/tty;'
+      .. 'tty="$(stty -g)"; stty sane; '
+      .. 'git ls-files -z --cached --other --exclude-standard'
+      .. '    | fzf --read0 --print0 --height=10'
+      .. '        --border=top --border-label-pos=1'
+      .. '        --color=border:7:reverse,label:7:reverse:bold'
+      .. '        --color=scrollbar:regular'
+      .. '        --layout=default --border-label=" '
+      .. (win.file.name .. string.rep(' ', win.width - #win.file.name - 2))
+      .. ' "; r=$?; stty "$tty">/dev/tty; exit $r'
+  )
+  if fz then
+    local out = fz:read('*a')
+    local _, _, status = fz:close()
+    if status == 0 then
+      if lspc then
+        lspc.open_file(win, out, nil, nil, 'e')
+      else
+        vis:command(
+          ('e %s'):format(out:gsub('[\\\t "\']', '\\%1'):gsub('\n', '\\n'))
+        )
+      end
+      return
+    end
+  end
+  vis:redraw()
+end, 'Open file using fuzzy finder')
+vis:map(vis.modes.NORMAL, '<M-p>', function()
+  vis:command('fuzzy-open')
+end)
+vis:map(vis.modes.NORMAL, '<C-p>', function()
+  vis:command('fuzzy-open')
+end)
+
+vis:command_register('fuzzy-find', function(argv, force, win, sel, range)
+  local fz = io.popen(
+    "RFV_QUERY='"
+      .. vis.registers['/'][1]:sub(1, -2):gsub("'", "'\\''")
+      .. "'"
+      .. ' rfv; r=$?; tput smcup >/dev/tty; exit $r'
+  )
+  if fz then
+    local out = fz:read('*a')
+    local _, _, status = fz:close()
+    if status == 0 then
+      local C, P, R = vis.lpeg.C, vis.lpeg.P, vis.lpeg.R
+      local any, colon, num, nl = P(1), P(':'), R('09'), P('\n')
+      local query, file, line, col = vis.lpeg.match(
+        P(C(P(1 - nl) ^ 0) * nl)
+          * C((any - P(colon * num ^ 1 * colon * num ^ 1 * colon)) ^ 1)
+          * P(colon * C(num ^ 1) * colon * C(num ^ 1) * colon),
+        out
+      )
+      vis:redraw()
+      lspc.open_file(win, file, line, col, 'e')
+      vis.registers['/'] = { query }
+      return
+    end
+  end
+  vis:redraw()
+end, 'Find in containing repository using fuzzy finder with preview')
+vis:map(vis.modes.NORMAL, '<M-f>', function()
+  vis:command('fuzzy-find')
+end)
+vis:map(vis.modes.NORMAL, '<C-f>', function()
+  vis:command('fuzzy-find')
+end)
+
+vis:command_register(
+  'show-syntax',
+  function(argv, force, win, selection, range)
+    vis:info(vis.win.syntax)
+  end,
+  'Show syntax of current file'
+)
+
+vis:map(vis.modes.INSERT, '<M-Escape>', '<vis-mode-normal>')
+vis:map(vis.modes.VISUAL, '<M-Escape>', '<vis-mode-normal>')
+vis:map(vis.modes.INSERT, '<Delete>', function()
+  vis:command('/\n[\t ]*|./d')
+  vis:feedkeys('<vis-mode-insert>')
+end, 'Remove character to the right or join lines if at the end')
+vis:map(vis.modes.NORMAL, 'U', '<vis-redo>')
+
+-- Y and P for system clipboard
+vis:map(vis.modes.NORMAL, 'Y', '"*y')
+vis:map(vis.modes.NORMAL, 'P', '"*p')
+vis:map(vis.modes.VISUAL, 'Y', '"*y')
+vis:map(vis.modes.VISUAL, 'D', '"*d')
+vis:map(vis.modes.VISUAL, 'P', '"*p')
+
+vis:command_register('make', function(argv, force, win, selection, range)
+  vis:command('w')
+  local pos = win.selection.pos
+  local status, out, err = vis:pipe(
+    win.file,
+    { start = 0, finish = 0 },
+    'makedir="$(dirname "$(upwardfind . Makefile)")";'
+      .. 'cd "$makedir";'
+      .. 'printf "\\e[1A">"$TTY";'
+      .. 'tgt="$(<Makefile sed -n "s/^.PHONY://p" | tr " " "\\n"'
+      .. '    | vis-menu -p make)" || exit 1;'
+      .. 'printf "\r\\e[1A\\e[2K\\e[7m make %s  " "$tgt">"$TTY";'
+      .. '(x="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"; while :; do '
+      .. '    printf "\b%.1s" "$x"; x=${x#?}${x%?????????}; sleep .1;'
+      .. 'done)>"$TTY"&'
+      .. 'make "$tgt";'
+      .. 'printf "\\e[?7h"; kill $!'
+  )
+  if status ~= 0 or not out then
+    if err then
+      vis:info(err)
+    end
+    return
+  end
+  vis:command('e')
+  vis:redraw()
+  win.selection.pos = pos
+end, 'Run make target')
+vis:map(vis.modes.NORMAL, 'm', function()
+  vis:command('make')
+end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
   vis:command('set theme default')
@@ -183,144 +331,6 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
   elseif (win.file.name or ''):match('/workspace/config$') then
     set_syntax('bash')
   end
-
-  local format_multiplex = function(file, range, pos)
-    if format.formatters[vis.win.syntax] then
-      vis.win.selection.pos = format.apply(file, range, pos)
-    elseif lspc then
-      local lspc_conf = lspc.ls_map[win.syntax]
-      if lspc_conf then
-        local ls = lspc.running[lspc_conf.name]
-        if ls and ls.capabilities['documentFormattingProvider'] then
-          vis:command('lspc-format')
-        end
-      end
-    else
-      vis:info('No formatter for ' .. vis.win.syntax)
-    end
-  end
-  vis:operator_new('=', format_multiplex)
-  vis:map(vis.modes.NORMAL, '=', function()
-    format_multiplex(vis.win.file, nil, vis.win.selection.pos)
-  end)
-
-  vis:map(vis.modes.INSERT, '<M-Escape>', '<vis-mode-normal>')
-  vis:map(vis.modes.VISUAL, '<M-Escape>', '<vis-mode-normal>')
-  vis:map(vis.modes.INSERT, '<Delete>', function()
-    vis:command('/\n[\t ]*|./d')
-    vis:feedkeys('<vis-mode-insert>')
-  end, 'Remove character to the right or join lines if at the end')
-  vis:map(vis.modes.NORMAL, 'U', '<vis-redo>')
-
-  -- Y and P for system clipboard
-  vis:map(vis.modes.NORMAL, 'Y', '"*y')
-  vis:map(vis.modes.NORMAL, 'P', '"*p')
-  vis:map(vis.modes.VISUAL, 'Y', '"*y')
-  vis:map(vis.modes.VISUAL, 'D', '"*d')
-  vis:map(vis.modes.VISUAL, 'P', '"*p')
-
-  -- make integration
-  local make = function()
-    local status, out, err = vis:pipe(
-      win.file,
-      { start = 0, finish = 0 },
-      'makedir="$(dirname "$(upwardfind . Makefile)")";'
-        .. 'cd "$makedir";'
-        .. 'printf "\\e[1A">"$TTY";'
-        .. 'tgt="$(<Makefile sed -n "s/^.PHONY://p" | tr " " "\\n"'
-        .. '    | vis-menu -p make)" || exit 1;'
-        .. 'printf "\r\\e[1A\\e[2K\\e[7m make %s  " "$tgt">"$TTY";'
-        .. '(x="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"; while :; do '
-        .. '    printf "\b%.1s" "$x"; x=${x#?}${x%?????????}; sleep .1;'
-        .. 'done)>"$TTY"&'
-        .. 'make "$tgt";'
-        .. 'printf "\\e[?7h"; kill $!'
-    )
-    if status ~= 0 or not out then
-      if err then
-        vis:info(err)
-      end
-      return
-    end
-  end
-  vis:map(vis.modes.NORMAL, 'm', function()
-    vis:command('w')
-    local pos = win.selection.pos
-    make()
-    vis:command('e')
-    vis:redraw()
-    win.selection.pos = pos
-  end, 'Run make command')
-
-  vis:command_register('fuzzy-open', function(argv, force, win, sel, range)
-    local fz = io.popen(
-      'tput cup $(( $(tput lines) - 10)) >/dev/tty;'
-        .. 'tty="$(stty -g)"; stty sane; '
-        .. 'git ls-files -z --cached --other --exclude-standard'
-        .. '    | fzf --read0 --print0 --height=10'
-        .. '        --border=top --border-label-pos=1'
-        .. '        --color=border:7:reverse,label:7:reverse:bold'
-        .. '        --color=scrollbar:regular'
-        .. '        --layout=default --border-label=" '
-        .. (win.file.name .. string.rep(' ', win.width - #win.file.name - 2))
-        .. ' "; r=$?; stty "$tty">/dev/tty; exit $r'
-    )
-    if fz then
-      local out = fz:read('*a')
-      local _, _, status = fz:close()
-      if status == 0 then
-        if lspc then
-          lspc.open_file(win, out, nil, nil, 'e')
-        else
-          vis:command(
-            ('e %s'):format(out:gsub('[\\\t "\']', '\\%1'):gsub('\n', '\\n'))
-          )
-        end
-        return
-      end
-    end
-    vis:redraw()
-  end, 'Open file using fuzzy finder')
-  vis:map(vis.modes.NORMAL, '<M-p>', function() vis:command('fuzzy-open') end)
-  vis:map(vis.modes.NORMAL, '<C-p>', function() vis:command('fuzzy-open') end)
-
-  vis:command_register('fuzzy-find', function(argv, force, win, sel, range)
-    local fz = io.popen(
-      "RFV_QUERY='"
-        .. vis.registers['/'][1]:sub(1, -2):gsub("'", "'\\''")
-        .. "'"
-        .. ' rfv; r=$?; tput smcup >/dev/tty; exit $r'
-    )
-    if fz then
-      local out = fz:read('*a')
-      local _, _, status = fz:close()
-      if status == 0 then
-        local C, P, R = vis.lpeg.C, vis.lpeg.P, vis.lpeg.R
-        local any, colon, num, nl = P(1), P(':'), R('09'), P('\n')
-        local query, file, line, col = vis.lpeg.match(
-          P(C(P(1 - nl) ^ 0) * nl)
-            * C((any - P(colon * num ^ 1 * colon * num ^ 1 * colon)) ^ 1)
-            * P(colon * C(num ^ 1) * colon * C(num ^ 1) * colon),
-          out
-        )
-        vis:redraw()
-        lspc.open_file(win, file, line, col, 'e')
-        vis.registers['/'] = { query }
-        return
-      end
-    end
-    vis:redraw()
-  end, 'Find in containing repository using fuzzy finder with preview')
-  vis:map(vis.modes.NORMAL, '<M-f>', function() vis:command('fuzzy-find') end)
-  vis:map(vis.modes.NORMAL, '<C-f>', function() vis:command('fuzzy-find') end)
-
-  vis:command_register(
-    'show-syntax',
-    function(argv, force, win, selection, range)
-      vis:info(vis.win.syntax)
-    end,
-    'Show syntax of current file'
-  )
 
   vis:command(
     string.format(

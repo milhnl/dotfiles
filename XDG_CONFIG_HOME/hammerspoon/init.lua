c1135761ff87a27f34fb5c1aa0d898c4b7925dcd
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2024-02-25 11:48:09 +0100

    Override some Terminal.app ⌘ shortcuts

--- XDG_CONFIG_HOME/hammerspoon/init.lua
+++ XDG_CONFIG_HOME/hammerspoon/init.lua
@@ -17,6 +17,39 @@ hs.hotkey.bind({ 'cmd' }, 'I', function()
   hs.application.launchOrFocusByBundleID('com.apple.Safari')
 end)
 
+-- Set to variable to avoid GC
+cmdTerminal = hs.eventtap
+  .new({ hs.eventtap.event.types.keyDown }, function(e)
+    if e:getFlags():containExactly({ 'cmd' }) then
+      if
+        e:getKeyCode() == hs.keycodes.map.p
+        and hs.application.frontmostApplication():bundleID()
+          == 'com.apple.Terminal'
+      then
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.p, false):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.cmd, false):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, true):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.p, true):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.p, false):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, false):post()
+        return true
+      elseif
+        e:getKeyCode() == hs.keycodes.map.f
+        and hs.application.frontmostApplication():bundleID()
+          == 'com.apple.Terminal'
+      then
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.f, false):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.cmd, false):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, true):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.f, true):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.f, false):post()
+        hs.eventtap.event.newKeyEvent(hs.keycodes.map.ctrl, false):post()
+        return true
+      end
+    end
+  end)
+  :start()
+
 -- Cmd + D
 hs.hotkey.bind({ 'cmd' }, 'D', function()
   hs.application.launchOrFocusByBundleID('com.microsoft.vscode')

b955d480617820b83604e50a95b9d9f4c559c5c6
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2025-11-14 16:27:09 +0100

    Fix shellcheck issues in profile.sh

--- XDG_CONFIG_HOME/sh/profile.sh
+++ XDG_CONFIG_HOME/sh/profile.sh
@@ -1,14 +1,16 @@
 # sh/profile.sh - session for POSIX shells
+# shellcheck disable=SC2034,SC2155
 append_path() {
     case "$PATH" in
-    "$1"|*":$1"|*":$1:"*) true ;;
+    "$1" | *":$1" | *":$1:"*) true ;;
     *) PATH="${PATH+$PATH:}$1" ;;
     esac
 }
 
 for x in /etc/profile.d/*.sh "$XDG_CONFIG_HOME/profile.d"/*.sh; do
     [ -e "$x" ] || continue
-    echo "$x" | grep -Eq jre\|perl\|raspberry || . "$x";
+    # shellcheck source=/dev/null
+    echo "$x" | grep -Eq jre\|perl\|raspberry || . "$x"
 done
 
 # ENVIRONMENT -----------------------------------------------------------------
@@ -30,8 +32,9 @@ XDG_BIN_HOME="${XDG_BIN_HOME-$PREFIX/bin}"
 XDG_DATA_HOME="${XDG_DATA_HOME-$PREFIX/share}"
 XDG_STATE_HOME="${XDG_DATA_HOME-$PREFIX/state}"
 PATH="$XDG_BIN_HOME:$PATH"
+# shellcheck source=../environment.d/10-applications.conf
 . "$XDG_CONFIG_HOME/environment.d/10-applications.conf"
-while read LINE; do
+while read -r LINE; do
     printenv "${LINE%%=*}" >/dev/null 2>&1 || eval "$LINE"
 done <"$XDG_CONFIG_HOME/user-dirs.dirs"
 if command -v python3 >/dev/null 2>&1; then
@@ -62,43 +65,51 @@ fi
 
 if grep -iq microsoft /proc/version 2>/dev/null; then
     # Extend PATH for ssh to WSL
-    append_path "$(cd /mnt/c; ./Windows/System32/cmd.exe /c 'echo %PATH%' \
-        | tr ';' '\n' \
-        | grep . \
-        | (
-            while IFS= read -r REPLY \
-                    && [ -n "$(echo "$REPLY"| tr -d '[:space:]')" ]; do
-                wslpath -u "$REPLY"
-            done
-        ) \
-        | tr '\n' ':')"
+    append_path "$(
+        cd /mnt/c || return
+        ./Windows/System32/cmd.exe /c 'echo %PATH%' \
+            | tr ';' '\n' \
+            | grep . \
+            | (
+                while IFS= read -r REPLY \
+                    && [ -n "$(echo "$REPLY" | tr -d '[:space:]')" ]; do
+                    wslpath -u "$REPLY"
+                done
+            ) \
+            | tr '\n' ':'
+    )"
     ! "${WSL_OWNER-false}" || export WORKSPACE_REPO_HOME="$(
         wslpath -u "$(powershell.exe -NoProfile -c '
             [Environment]::GetEnvironmentVariable("WORKSPACE_REPO_HOME")
-        ' | sed s/\\r\$//)")"
+        ' | sed s/\\r\$//)"
+    )"
 elif [ "$(uname -s)" = Darwin ]; then
-    export HOMEBREW_PREFIX="/opt/homebrew";
-    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
-    export HOMEBREW_REPOSITORY="/opt/homebrew";
-    export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
-    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+    export HOMEBREW_PREFIX="/opt/homebrew"
+    export HOMEBREW_CELLAR="/opt/homebrew/Cellar"
+    export HOMEBREW_REPOSITORY="/opt/homebrew"
+    export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:"
+    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}"
     append_path "/opt/homebrew/bin:/opt/homebrew/sbin"
 fi
 
 append_path "$PREFIX/lib/sh/lazyload"
 
 if command -v fzf 2>/dev/null | grep -qv /lazyload/ \
-        && fzf --help | grep -q .--pointer; then
+    && fzf --help | grep -q .--pointer; then
     FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --pointer=' '"
 fi
 
 # SSH/GPG ---------------------------------------------------------------------
-(set -- gnome3 mac curses; until command -v pinentry-$1; do
-        shift; [ $# -gt 0 ] || break;
-    done) 2>/dev/null \
+(
+    set -- gnome3 mac curses
+    until command -v "pinentry-$1"; do
+        shift
+        [ $# -gt 0 ] || break
+    done
+) 2>/dev/null \
     | sed 's/^/pinentry-program /' \
     | cat "$GNUPGHOME/gpg-agent-base.conf" - \
-    >"$GNUPGHOME/gpg-agent.conf" 2>/dev/null
+        >"$GNUPGHOME/gpg-agent.conf" 2>/dev/null
 
 # Set-up ----------------------------------------------------------------------
 mergehistory() {
@@ -110,9 +121,9 @@ mergehistory() {
 }
 
 [ -f "$HOME/.bash_history" ] \
-    && (mergehistory "$HOME/.bash_history" "$XDG_DATA_HOME/bash/history"&)
+    && (mergehistory "$HOME/.bash_history" "$XDG_DATA_HOME/bash/history" &)
 [ -f "$HOME/.zsh_history" ] \
-    && (mergehistory "$HOME/.zsh_history" "$XDG_DATA_HOME/zsh/history"&)
+    && (mergehistory "$HOME/.zsh_history" "$XDG_DATA_HOME/zsh/history" &)
 
 # Start X ---------------------------------------------------------------------
 [ -n "${ALACRITTY_SOCKET-}" ] \
